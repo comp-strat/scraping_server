1ac5d95a41219146da78e486ad590e2a6e984e34
Artem <artemtkachuk@yahoo.com>
2021-04-21 22:19:09 +0300

    Change react structure

---
+++ client/src/components/Components.js
@@ -0,0 +1,221 @@
+import React, { Component } from "react";
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
+import AssignmentTurnedInRoundedIcon from '@material-ui/icons/AssignmentTurnedInRounded';
+import AssignmentRoundedIcon from '@material-ui/icons/AssignmentRounded';
+import ErrorIcon from '@material-ui/icons/Error';
+import SettingsIcon from '@material-ui/icons/Settings';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Link from '@material-ui/core/Link';
+import pranav from '../static/img/pranav.png';
+
+const drawerWidth = 250;
+
+const useStyles = makeStyles({
+
+  user: {
+    margin: '20px',
+    alignItems: 'flex-start',
+  },
+
+  userName: {
+    alignItems: 'flex-start',
+  },
+
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+
+  drawerPaper: {
+    width: drawerWidth,
+  },
+});
+
+const handleDrawerClick = (props, suffix) => {
+  const JobsPath = '/jobs';
+  const DashboardPath = '/dashboard'
+  const DatasetPath = '/datasets'
+  var path;
+  if (suffix == 'jobs') {
+		path = JobsPath;
+	} else if (suffix == 'dashboard') {
+		path = DashboardPath;
+	} else if (suffix == 'datasets') {
+		path = DatasetPath;
+	}
+  props.history.push({
+    pathname: path,
+  });
+}
+
+export function Copyright(props) {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/URAP-charter">
+        The Universal Web Crawling Team
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export function User(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.user}>
+      <Avatar src={pranav} />
+      <Typography component="div" variant="h6" component="h6" className={classes.userName}>
+        <Box fontWeight="fontWeightBold" m={0}>
+          {props.name}
+        </Box>
+      </Typography>
+    </div>
+  );
+}
+
+export function DrawerButton(props) {
+  return (
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <ListItemText
+        disableTypography
+        primary={
+          <Typography component="div">
+            <Box fontWeight="fontWeightBold" m={0}>
+              {props.name}
+            </Box>
+          </Typography>
+        }
+      />
+    </ListItem>
+  );
+}
+
+export function LeftDrawer(props) {
+  const classes = useStyles();
+  return (
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <User name="Pranav Bhasin"/>
+      <Divider />
+      <List>
+        <ListItem button onClick={() => handleDrawerClick(props, "dashboard")}>
+          <ListItemIcon>
+            <DashboardRoundedIcon />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography component="div">
+                <Box fontWeight="fontWeightBold" m={0}>
+                  Dashboard
+                </Box>
+              </Typography>
+            }
+          />
+        </ListItem>
+        <ListItem button onClick={() => handleDrawerClick(props, "jobs")}>
+          <ListItemIcon>
+            <AssignmentTurnedInRoundedIcon />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography component="div">
+                <Box fontWeight="fontWeightBold" m={0}>
+                  Jobs
+                </Box>
+              </Typography>
+            }
+          />
+        </ListItem>
+        <ListItem button onClick={() => handleDrawerClick(props, "datasets")}>
+          <ListItemIcon>
+            <AssignmentRoundedIcon />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography component="div">
+                <Box fontWeight="fontWeightBold" m={0}>
+                  Datasets
+                </Box>
+              </Typography>
+            }
+          />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <ErrorIcon />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography component="div">
+                <Box fontWeight="fontWeightBold" m={0}>
+                  Errors
+                </Box>
+              </Typography>
+            }
+          />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+      <ListSubheader inset>Settings</ListSubheader>
+        <ListItem button>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography component="div">
+                <Box fontWeight="fontWeightBold" m={0}>
+                  Settings
+                </Box>
+              </Typography>
+            }
+          />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <NotificationsIcon />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography component="div">
+                <Box fontWeight="fontWeightBold" m={0}>
+                  Notifications
+                </Box>
+              </Typography>
+            }
+          />
+        </ListItem>
+      </List>
+    </Drawer>
+  )
+}
---
+++ client/src/pages/Datasets.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import { Box, Grid, Typography } from '@material-ui/core';
+import {Copyright, LeftDrawer} from '../components/Components.js'
+import {pesudoJobs} from '../data/pesudoData.js'
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ShareIcon from '@material-ui/icons/Share';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from "@material-ui/core";
+
+import Avatar from '@material-ui/core/Avatar';
+import pranav from '../static/img/pranav.png';
+import charlotte from '../static/img/Charlotte.jpg'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+
+  exampleCardStyle: {
+    maxWidth: 300,
+  },
+
+  exampleCardImageStyle: {
+    height: 150,
+  },
+
+  main: {
+    flexGrow: 1,
+  },
+
+  datasets: {
+    paddingTop: theme.spacing(2),
+    paddingRight: theme.spacing(8),
+    paddingLeft: theme.spacing(8),
+  },
+
+}));
+
+function Title(props) {
+  return (
+    <Typography component="h2" variant="h4" color="primary" gutterBottom>
+      {props.children}
+    </Typography>
+  );
+}
+
+function Samples(props) {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title  children="Datasets" />
+      <Grid
+        container
+        justify="space-evenly"
+        spacing={3}
+        className={classes.datasets}
+      >
+        {pesudoJobs.map((job) => {
+          return (
+            <Grid item xs={2} md={3} sm={6} key={job.id}>
+              <Card className={classes.exampleCardStyle}>
+              <CardHeader
+                avatar={<Avatar src={pranav} />}
+                action={
+                  <IconButton>
+                    <ShareIcon />
+                  </IconButton>
+                }
+                title={job.creator}
+                subheader={job.date}
+              />
+                <CardMedia
+                  className={classes.exampleCardImageStyle}
+                  image={charlotte}
+                  title={job.ticket}
+                />
+                <CardContent>
+                  <Typography variant="h6" component="h2">
+                    {job.ticket}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button variant="outlined" color="primary">VIEW</Button>
+                  <Button variant="outlined" color="primary">EDIT</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </React.Fragment>
+  )
+}
+
+export function DatasetPage(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <LeftDrawer history={props.history}/>
+      <main className={classes.main}>
+        <Samples />
+        <Box pt={4}>
+          <Copyright />
+        </Box>
+      </main>
+    </div>
+  )
+}
+
+class Datasets extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <DatasetPage history={this.props.history}/>
+    )
+  }
+}
+
+export default Datasets;
