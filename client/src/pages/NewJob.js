817db638f45efd7b8f044f48b2125e48e53384bf
mathfather <mathfather7701@gmail.com>
2021-05-05 13:07:51 -0700

    Upload button and text field

--- client/src/pages/NewJob.js
+++ client/src/pages/NewJob.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import {LeftDrawer, Copyright} from '../components/Components.js'
 import { makeStyles } from "@material-ui/core";
 import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) =>({
   root: {
@@ -10,13 +14,61 @@ const useStyles = makeStyles((theme) =>({
 
   main: {
     flexGrow: 1,
-  }
+  },
+
+  textInput: {
+    padding: 15
+  },
+
+  input: {
+    display: 'none',
+  },
 
 }));
 
 function NewJobContent(props) {
+  const classes = useStyles();
   return (
-    <div />
+    <div>
+      <Grid
+        container
+        justify='center'
+        alignItems="stretch"
+        spacing={2}
+      >
+        <Grid item xs={5} />
+        <Grid item xs={2}>
+          <Paper component="form">
+            <TextField
+              placeholder="urls"
+              helperText="Seperated by comma"
+              margin="normal"
+              variant="outlined"
+              multiline
+              rows={3}
+              rowsMax={4}
+              style={{ margin: 8 }}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={5} />
+        <Grid item xs={5} />
+        <Grid item xs={2}>
+          <input
+            className={classes.input}
+            id="contained-button-file"
+            multiple
+            type="file"
+          />
+          <label htmlFor="contained-button-file">
+            <Button variant="contained" color="primary" component="span">
+              Upload
+            </Button>
+          </label>
+        </Grid>
+        <Grid item xs={5} />
+      </Grid>
+    </div>
   )
 }
 
