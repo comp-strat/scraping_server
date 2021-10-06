514445ffb5d3425b8eef29aaf90ab71c1b51b814
Dev Bali <devbali02@gmail.com>
2021-10-06 01:19:56 -0700

    Edited kill script

--- .kill.sh
+++ .kill.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo THIS SCRIPT KILLS ALL DOCKERS, PYTHON PROCESSES, NODE PROCESSES ON THIS MACHINE
 docker rm -f $(docker ps -a -q)
-pkill python
-pkill node
+ps -ax | grep -w python | awk -F ' ' '{print $1}' | xargs sudo kill -9
+ps -ax | grep -w node | awk -F ' ' '{print $1}' | xargs sudo kill -9
+
 # Kills all docker images stored in storage
 #docker system prune -a
