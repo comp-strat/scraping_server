6f40f1ac3ff71e41889ab34723e6d7a7dd284a70
Jaren Haber <jhaber-zz@users.noreply.github.com>
2021-12-08 14:19:54 -0500

    update OAuth Client ID to Jaren's
    Instructions: https://developers.google.com/identity/sign-in/web/sign-in#create_authorization_credentials
    Put the client ID in `client/src/server-config.js`

--- client/src/server-config.js
+++ client/src/server-config.js
@@ -2,7 +2,7 @@
 var config = {
     serverurl: process.env.REACT_APP_SERVER_URL || "http://localhost:5000",
     port: process.env.REACT_APP_PORT || 3000,
-    google_oauth_clientid: '1080632962111-av26u4euc5vgoollpmfbrtiissptg4dc.apps.googleusercontent.com'
+    google_oauth_clientid: '980011737294-kriddo55g39bja7timpfk233lm83l8jl.apps.googleusercontent.com'
 }
 
 export default config;
