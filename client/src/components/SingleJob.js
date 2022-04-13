59743857f79dbd360aa754477b9c8e3c65e9ba0e
Michael Lin <miclin@berkeley.edu>
2022-04-13 01:11:51 -0700

    Update client to use the new download api

--- client/src/components/SingleJob.js
+++ client/src/components/SingleJob.js
@@ -22,20 +22,6 @@ class SingleJob extends Component {
       .then(res => this.updateStatus());
   };
 
-  downloadFunc = () => {
-    fetchWithUserToken(`/api/jobs/${this.props.id}/files`, {method:"GET"})
-      .then(response => response.blob())
-      .then(blob => {
-        let url = window.URL.createObjectURL(blob);
-        let a = document.createElement("a");
-        a.href = url;
-        a.download = this.props.id + ".zip";
-        document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-        a.click();    
-        a.remove();  //afterwards we remove the element again         
-      });
-  };
-
   updateStatus = () => {
     fetchWithUserToken(`/api/jobs/${this.props.id}`, {method:"GET"})
       .then(res => {
@@ -89,7 +75,8 @@ class SingleJob extends Component {
               <CardActions><TopButton
                 variant="extended"
                 color="primary"
-                onClick={this.downloadFunc}>
+                href={`/api/jobs/${this.props.id}/download`}
+              >
                                 Download
               </TopButton></CardActions>
             </div>,
